87ec85519dcd9d19bf00d3cef126f77f76f3c7e6
Benhur Külzer <benhur.bitpc@gmail.com>
2020-05-19 00:37:37 -0300

    Routes, Controllers, Rules of schedule and provider schedule

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const apointmentsController = new AppointmentsController();
+const providerApointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', apointmentsController.create);
+appointmentsRouter.get('/me', providerApointmentsController.index);
 
 export default appointmentsRouter;
